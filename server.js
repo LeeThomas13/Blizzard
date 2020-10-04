f6e8491dc9e12fb086e0f7f9e8558b21e4eb43a7
LeeThomas13 <lmtthomasritz@gmail.com>
2020-10-04 14:09:21 -0700

    finished dependencies

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
 'use strict'
+
+require('dotenv').config();
+require('ejs');
+
+const cors = require('cors');
+const express = require('express');
+const methodOverride = require('method-override');
+const pg = require('pg');
