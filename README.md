e4c0c201792deff5c470d6a03d246b0948c24fb0
LeeThomas13 <lmtthomasritz@gmail.com>
2020-10-04 14:39:51 -0700

    finished skeleton for app

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Blizzard
+This app is for testing the blizzard API, to see what information is being thrown back to me, and that will drive the purpose of the app.
+
+My original goal was to make a 'homebase' app that would give you small snippets of your data from each blizzard game(diablo, hearthstone, wow, over watch) But upon seeing that call of duty was hosted by battlenet, I am excited to see if I can get into that data from the API.
--- server.js
+++ server.js
@@ -1,9 +1,39 @@
 'use strict'
 
+//configs
 require('dotenv').config();
 require('ejs');
 
+//dependencies
 const cors = require('cors');
 const express = require('express');
 const methodOverride = require('method-override');
 const pg = require('pg');
+const superagent = require('superagent');
+
+//app setup
+const app = express();
+app.use(cors());
+
+const PORT = process.env.PORT || 3001;
+const client = new pg.Client(process.env.DATABASE_URL);
+client.on('error', error => {
+    console.log(error);
+});
+
+//middleware 
+app.set('view engine', 'ejs');
+app.use(express.static('./public'));
+app.use(express.urlencoded({ extended: true}));
+app.use(methodOverride('_method'));
+
+//routes
+// app.get('/', renderHomePage);
+
+
+client.connect() 
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`we're listening on ${PORT}`);
+        })
+    })
